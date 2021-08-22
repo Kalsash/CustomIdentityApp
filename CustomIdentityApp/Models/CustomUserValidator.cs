3941a1fb7d3c3c00529af738ee0372b007b8dcc8
Aleksandr Kalinin <akalinin@sfedu.ru>
2021-08-22 22:16:32 +0300

    Добавил сообщение о том, что Email уже используется

--- CustomIdentityApp/Models/CustomUserValidator.cs
+++ CustomIdentityApp/Models/CustomUserValidator.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,6 +28,28 @@ namespace CustomIdentityApp.Models
                     Description = "Ник пользователя не должен содержать слово 'admin'"
                 });
             }
+            const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=usersstoredb;Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT UserName FROM AspNetUsers WHERE Year > 0", connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            List<string> list = new List<string>();
+        foreach (DataRow row in table.Rows)
+            {
+                list.Add(row[0].ToString());
+                //row[2].ToString(); //- к отдельной ячейке в указанной строке
+            }
+            foreach (var names in list)
+            {
+                if (user.UserName.Contains(names))
+                {
+                    errors.Add(new IdentityError
+                    {
+                        Description = "Этот Email уже используется! Введите какой-нибудь другой."
+                    });
+                }
+            }
+
             return Task.FromResult(errors.Count == 0 ?
                 IdentityResult.Success : IdentityResult.Failed(errors.ToArray()));
         }
